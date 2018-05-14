f46af7a86244079b4d8db5d212cb4dff7e7fa5e7
blsmac1 <blsmac20171@gmail.com>
2018-05-14 14:50:49 +0700

    basic done with app todolistapp

--- ToDoListApp/App.js
+++ ToDoListApp/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/main';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world!!!</Text>
-      </View>
+      <Main />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
