512fad79e7c0f8a8efdb57ffbc2281ffbf646a63
truphuonglong <longbaloca@gmail.com>
2018-05-16 19:43:32 +0700

    aaa

--- ToDoListApp/src/main.js
+++ ToDoListApp/src/main.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import {Provider} from 'react-redux';
-import {AppNavigator} from './config/routes';
-import {initApi} from './services/userApi';
+import { Provider } from 'react-redux';
+import { AppNavigator } from './config/routes';
+import { initApi } from './services/userApi';
 import storeTask from './redux/store/storeTask';
 
 export default class Main extends Component {
-    componentWillMount(){
+    componentWillMount() {
         initApi();
     }
     render() {
         return (
-            <AppNavigator />
+            <Provider store={storeTask}>
+                <AppNavigator />
+            </Provider>
         )
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        messages: state.messages
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        addMessage: (message) => {
+            dispatch(addMessage(message))
+        }
     }
 }
